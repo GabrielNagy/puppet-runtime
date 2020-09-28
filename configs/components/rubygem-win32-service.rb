99cf1b934dd2f45747cf2916c876ff62250aab08
Gabriel Nagy <gabriel.nagy@puppet.com>
2020-09-28 15:13:43 +0300

    (maint) Bump win32-service to 2.1.6
    The previous version of win32-service had a double-defined struct which
    caused FFI 1.13.1 to print a warning on stderr. Bump win32-service to
    the latest version which does not have this issue.

--- configs/components/rubygem-win32-service.rb
+++ configs/components/rubygem-win32-service.rb
@@ -1,6 +1,6 @@
 component "rubygem-win32-service" do |pkg, settings, platform|
-  pkg.version "0.8.8"
-  pkg.md5sum "24cc05fed398eb931e14b8ee22196634"
+  pkg.version "2.1.6"
+  pkg.sha256sum "ca2e68daf88c5be13d80142594be81de9da020003190a72706190c3a67d45135"
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
