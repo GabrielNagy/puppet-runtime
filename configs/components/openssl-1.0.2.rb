178b3eaa37097c65911fc88d1b383b405d5e1dbb
Gabriel Nagy <gabriel.nagy@puppet.com>
2020-11-10 17:15:37 +0200

    (maint) Build OpenSSL with dynamicbase on Windows FIPS

--- configs/components/openssl-1.0.2.rb
+++ configs/components/openssl-1.0.2.rb
@@ -127,7 +127,7 @@ component 'openssl' do |pkg, settings, platform|
     'no-ssl3',
   ]
 
-  configure_flags += ['fips', "--with-fipsdir=#{settings[:prefix]}/usr/local/ssl/fips-2.0"] if platform.name =~ /windowsfips-2012r2/
+  configure_flags += ['fips', "--with-fipsdir=#{settings[:prefix]}/usr/local/ssl/fips-2.0", "-Wl,--dynamicbase"] if platform.name =~ /windowsfips-2012r2/
 
   # Individual projects may provide their own openssl configure flags:
   project_flags = settings[:openssl_extra_configure_flags] || []
